54cf828cca7aa73c1e8215aa99f464a367a2398c
paul <paul@linux>
2020-02-12 21:21:22 +0100

    Now the examplecommand has a function.

--- src/deskplaner/tool/example/commands/ExampleCommand.java
+++ src/deskplaner/tool/example/commands/ExampleCommand.java
@@ -1,16 +1,20 @@
 package deskplaner.tool.example.commands;
 
+import java.util.Arrays;
 import deskplaner.util.Command;
 
 public class ExampleCommand implements Command {
 
 	/**
+	 * The examplecommand shows the arguments on the console.<br><br>
+	 * <i>Der Beispielcommand zeigt die Argumente auf der Konsole.</i>
+	 * 
 	 * @see deskplaner.util.Command#onCommand(String[] args)
 	 * @author Paul Leppich
 	 */
 	@Override
 	public boolean onCommand(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(" arguments: " + Arrays.toString(args));
 		return true;
 	}
 
